1f8e75c56a04c59d53310d22a2bd23973e397ae2
sergi <sergi@unievegroup.com>
2020-08-03 00:32:18 +0200

    Added test for when there aren't enough images at pixabay

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,7 @@ import App from './App';
 
 test('Check that images are shown after keyword', () => {
     const utils = render(<App />);
-    const inputElement = utils.getByPlaceholderText('Keyword');
+    const inputElement = utils.getByPlaceholderText('Enter Keyword');
     expect(inputElement).toBeInTheDocument();
     fireEvent.change(inputElement, {target: {value: 'card'}});
     expect(inputElement.value).toBe('card');
@@ -17,4 +17,22 @@ test('Check that images are shown after keyword', () => {
             expect(imageElement).toBeInTheDocument();
         }
     }, 1000)
-})
+});
+
+test('check that alert message pops up when not enough images', () => {
+    const utils = render(<App />);
+    const inputElement = utils.getByPlaceholderText('Enter Keyword');
+    expect(inputElement).toBeInTheDocument();
+    fireEvent.change(inputElement, {target: {value: 'somethingthatwontgetimages'}});
+    expect(inputElement.value).toBe('somethingthatwontgetimages');
+    const submitButtonElement = utils.getByText('Start Game!');
+    expect(submitButtonElement).toBeInTheDocument();
+    submitButtonElement.click();
+    setTimeout(()=>{
+        for(let i = 0; i < 20; i++){
+            const imageElement = utils.getByTestId(i);
+            expect(imageElement).not.toBeInTheDocument();
+        }
+    }, 1000)
+});
+

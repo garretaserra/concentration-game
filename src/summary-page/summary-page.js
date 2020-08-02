32fcbc679226a90e245a2bda20b362c10caa3f30
sergi <sergi@unievegroup.com>
2020-08-02 22:10:24 +0200

    Center elements

--- src/summary-page/summary-page.js
+++ src/summary-page/summary-page.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './summary-page.css'
 
 class SummaryPage extends React.Component{
     render() {
         return(
-            <div>
+            <div className="container">
                 <h2>Your time was {this.props.time} seconds</h2>
                 <button onClick={this.props.sameKeyword}>Go Again!</button>
                 <button onClick={this.props.newKeyword}>New Keyword</button>
